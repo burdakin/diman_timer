74fa6b690458526c9b3144a455ef78650cd28261
burdakin <serjb94@gmail.com>
2021-02-12 20:31:15 +0300

    Uploaded current version

--- index.js
+++ index.js
@@ -64,45 +64,6 @@ function day_word(n) {
         case 51:
             return 'день';
             break;
-
-        case 5:
-        case 6:
-        case 7:
-        case 8:
-        case 9:
-        case 10:
-        case 0:
-        case 11:
-        case 12:
-        case 13:
-        case 14:
-        case 15:
-        case 16:
-        case 17:
-        case 18:
-        case 19:
-        case 20:
-        case 25:
-        case 26:
-        case 27:
-        case 28:
-        case 29:
-        case 30:
-        case 35:
-        case 36:
-        case 37:
-        case 38:
-        case 39:
-        case 40:
-        case 45:
-        case 46:
-        case 47:
-        case 48:
-        case 49:
-        case 50:
-            return 'дней';
-            break;
-
         case 2:
         case 3:
         case 4:
@@ -143,24 +104,7 @@ function hrs_word(n) {
             return 'часа';
             break;
 
-        case 0:
-        case 5:
-        case 6:
-        case 7:
-        case 8:
-        case 9:
-        case 10:
-        case 11:
-        case 12:
-        case 13:
-        case 14:
-        case 15:
-        case 16:
-        case 17:
-        case 18:
-        case 19:
-        case 20:
-            return 'часов';
+            default return 'часов';
             break;
     }
 }
